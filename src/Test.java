0ba79c7acd985aeb6a759bc0184ab40bf3f949b4
Małgorzata Pierzchała <gk.kalinowska@gmail.com>
2018-08-17 18:34:57 +0200

    poprawki

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.Scanner;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Test {
     public static void main(String[] args) throws IOException {
@@ -11,24 +9,35 @@ public class Test {
         File file = new File("liczby.txt");
         file.createNewFile();
 
-        TreeSet<Integer> set = new TreeSet<>();
+        TreeMap<Integer, Integer> numbers = new TreeMap<>();
 
         try{
             Scanner scan = new Scanner(file);
+            int iloscWystapien;
             while(scan.hasNextInt()){
             int liczba = scan.nextInt();
-            set.add(liczba);
+            if(numbers.containsKey(liczba)){
+                iloscWystapien = numbers.get(liczba) + 1;
+            } else {
+                iloscWystapien = 1;
+            }
+            numbers.put(liczba, iloscWystapien);
             }
         } catch (FileNotFoundException e){
             e.printStackTrace();
         }
 
-        Iterator <Integer> iterator = set.iterator();
-        while(iterator.hasNext()){
-            Integer next = iterator.next();
-            int iloscWystapien = Method.checkIfContains(next, file);
-            System.out.println(next + " - liczba wystąpień: " + iloscWystapien);
-        }
-    }
+        final Set<Integer> integers = numbers.keySet();
+        final Collection<Integer> values = numbers.values();
 
+        Iterator<Integer> iterator1 = integers.iterator();
+        Iterator<Integer> iterator2 = values.iterator();
+
+        while (iterator1.hasNext() && iterator2.hasNext()){
+            Integer number1 = iterator1.next();
+            Integer number2 = iterator2.next();
+            System.out.println(number1 + " - liczba wystąpień: " + number2);
+        }
+
+    }
 }
